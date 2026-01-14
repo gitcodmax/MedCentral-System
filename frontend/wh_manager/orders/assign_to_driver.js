bde5e5032928bc54732b416c1048e76ec35a6baf
Max <mugunamaxwel@gmail.com>
2026-01-14 10:21:53 +0300

    Set up the filtering logic and container

--- frontend/wh_manager/orders/assign_to_driver.js
+++ frontend/wh_manager/orders/assign_to_driver.js
@@ -3,6 +3,102 @@ import { xRemoveOverlay, clickToRemoveOverlay } from "./overlay.js";
 
 document.addEventListener('DOMContentLoaded', () => {
 
+  document.querySelector('.page-container')
+    .innerHTML = `
+      <nav class="sidebar"></nav>
+
+      <main class="main-content">
+        <header class="logo-container"></header>
+
+        <h2>Dispatch Center</h2>
+        <div class="stat"> <span class="js-no-of-orders"></span> Packages Ready to Ship</div>
+
+        <div class="search-dashboard">
+          <div class="search-row">
+            <div class="search-group flex-2">
+              <label><i class="fas fa-hospital"></i> Organization / Package ID</label>
+              <input type="text" id="idSearch" placeholder="e.g. Nairobi Hospital or ORD-101-R">
+            </div>
+
+            <div class="search-group flex-1">
+              <label><i class="fas fa-map-marked-alt"></i> County</label>
+              <select id="countyFilter">
+                <option value="">All Counties</option>
+                <option value="Nairobi">Nairobi</option>
+                <option value="Kiambu">Kiambu</option>
+                <option value="Machakos">Machakos</option>
+              </select>
+            </div>
+
+            <div class="search-group flex-1">
+              <label><i class="fas fa-map-pin"></i> Zone (Sub-County)</label>
+              <select id="zoneFilter">
+                <option value="">All Zones</option>
+                <option value="Upper Hill">Upper Hill</option>
+                <option value="Parklands">Parklands</option>
+                <option value="Kikuyu">Kikuyu</option>
+              </select>
+            </div>
+          </div>
+
+          <div class="temp-filter-row">
+            <span>Storage Condition:</span>
+            <div class="temp-options">
+              <button class="t-btn" data-temp="A"><i class="fa-solid fa-house-medical-circle-check"></i> Ambient</button>
+              <button class="t-btn" data-temp="C"><i class="fas fa-thermometer-half"></i> CRT</button>
+              <button class="t-btn" data-temp="R"><i class="fas fa-snowflake"></i> Ref</button>
+              <button class="t-btn" data-temp="F"><i class="fas fa-icicles"></i> Frozen</button>
+            </div>
+            <button class="reset-btn js-reset-btn">Reset All</button>
+          </div>
+        </div>
+
+        <section class="dispatch-grid js-dispatch-grid"></section>
+
+        <div class="overlay" id="items-overlay">
+          <div class="items-preview-container">
+            <div class="header-close-container">
+              <div class="preview-header">
+                <span class="order-package-ids js-order-package-ids"></span> for
+                <span class="org-name js-org-name">Kenyatta Hospital</span>
+              </div>
+              <div>
+                <button class="close-overlay-btn js-close-overlay-btn">
+                  <i class="fas fa-times"></i>
+                </button>
+              </div>
+            </div>
+            <table>
+              <thead>
+                <th>No.</th>
+                <th>ITEM NAME</th>
+                <th>REQUESTED QUANTITY</th>
+              </thead>
+              <tbody id="items-tbody"></tbody>
+            </table>
+          </div>
+        </div>
+
+        <div class="overlay" id="dispatch-overlay">
+          <div class="notification-container">
+            <div class="modal-body">
+              <p>Assign
+                <span class="highlight-order js-pkg-id"></span>,
+                <span class="dispatch-org-name js-dispatch-org-name"></span>
+                to
+                <span class="highlight-driver js-driver-name"></span>?
+              </p>
+            </div>
+
+            <div class="modal-footer">
+              <button class="btn-no js-btn-no">No, Cancel</button>
+              <button class="btn-yes">Yes, Assign</button>
+            </div>
+          </div>
+        </div>
+      </main>
+    `
+
   renderSidebar()
 
   const orderDriverData = {
@@ -142,26 +238,35 @@ document.addEventListener('DOMContentLoaded', () => {
     ]
   }
 
+  const pkgToShipElem = document.querySelector('.js-no-of-orders')
+  const dispatchGridElem = document.querySelector('.js-dispatch-grid')
   const dispatchOdersFragment = document.createDocumentFragment()
 
-  //Display all the packages in the page
-  orderDriverData['dispatchQueue'].forEach(order => {
-    const orderPackage = order['packages']
+  //Find the number of packages awaiting dispatch
+  function getNoOfPackages() {
+    let noOfPackages = 0
+    orderDriverData['dispatchQueue'].forEach(ord => {
+      noOfPackages += ord.packages.length
+    })
 
-    orderPackage.forEach(pkg => {
-      const orderCard = document.createElement('div')
-      orderCard.className = 'dispatch-card js-dispatch-card'
+    return noOfPackages
+  }
 
-      let storageTempIcon = 'fa-solid fa-house-medical-circle-check'
-      if (pkg.storageTemp === 'crt') {
-        storageTempIcon = `fas fa-thermometer-half`
-      } else if (pkg.storageTemp === 'refrigerated') {
-        storageTempIcon = `fas fa-snowflake`
-      } else if (pkg.storageTemp === 'frozen') {
-        storageTempIcon = `fas fa-icicles`
-      }
+  // Generates the html for each of the cards in the page
+  function displayPackages(pkg, order) {
+    const orderCard = document.createElement('div')
+    orderCard.className = 'dispatch-card js-dispatch-card'
 
-      orderCard.innerHTML = `
+    let storageTempIcon = 'fa-solid fa-house-medical-circle-check'
+    if (pkg.storageTemp === 'crt') {
+      storageTempIcon = `fas fa-thermometer-half`
+    } else if (pkg.storageTemp === 'refrigerated') {
+      storageTempIcon = `fas fa-snowflake`
+    } else if (pkg.storageTemp === 'frozen') {
+      storageTempIcon = `fas fa-icicles`
+    }
+
+    orderCard.innerHTML = `
         <div class="card-badge"><i class="${storageTempIcon}"></i></div>
         <div class="card-main">
           <div class="order-meta">
@@ -200,12 +305,79 @@ document.addEventListener('DOMContentLoaded', () => {
         </div>
       `
 
-      dispatchOdersFragment.appendChild(orderCard)
-    })
-  })
+    dispatchOdersFragment.appendChild(orderCard)
+  }
 
-  document.querySelector('.js-dispatch-grid')
-    .appendChild(dispatchOdersFragment)
+  //Function to control how the drivers are displayed in the the select driver drop down
+  function displayDrivers(packageId, storageTemp, packageWt, subCnty, cnty) {
+    const selectElem = document.querySelector(`.js-driver-select-${packageId}`);
+    if (!selectElem) return;
+
+    const selectDriverFragment = document.createDocumentFragment();
+
+    orderDriverData.drivers.forEach(driver => {
+      const vehicleData = driver.vehicle
+      const { tempCaps, currentLoad, maxTons } = vehicleData
+      const { primaryZone, homeCounty } = driver
+
+      if (!tempCaps.includes(storageTemp)) {
+        return;
+      }
+
+      const totalLoad = Number((currentLoad + packageWt).toFixed(2))
+      if (totalLoad >= maxTons) {
+        return;
+      }
+
+      const zoneMatch = primaryZone.toLowerCase() === subCnty.toLowerCase();
+      const countyMatch = homeCounty.toLowerCase() === cnty.toLowerCase();
+      const option = document.createElement('option');
+      option.value = driver.driverId;
+
+      let status = ''
+      if (zoneMatch && countyMatch) {
+        status = '🟢Zone 🟢County'
+      } else if (zoneMatch) {
+        status = '🟢Zone 🔵County'
+      } else if (countyMatch) {
+        status = '🔵Zone 🟢County'
+      } else {
+        status = '🔵Zone 🔵County'
+      }
+
+      option.textContent = `${driver.name} @ ${currentLoad}t | ${status}`;
+      selectDriverFragment.appendChild(option)
+    })
+
+    selectElem.appendChild(selectDriverFragment)
+  }
+
+  //Displays the packages in the html
+  function displayAllPackages(orderDriverData) {
+    pkgToShipElem.textContent = getNoOfPackages()
+    dispatchGridElem.innerHTML = ``
+
+    orderDriverData['dispatchQueue'].forEach(ord => {
+      ord['packages'].forEach(pkg => {
+        displayPackages(pkg, ord)
+      })
+    })
+
+    // Display all the cards that have been stored in the fragment to the page
+    dispatchGridElem.appendChild(dispatchOdersFragment)
+  }
+
+  displayAllPackages(orderDriverData)
+  displayRightDrivers(orderDriverData)
+
+  function displayRightDrivers(orderDriverData) {
+    orderDriverData['dispatchQueue'].forEach(ord => {
+      ord['packages'].forEach(pkg => {
+        displayDrivers(pkg.packageId, pkg.storageTemp,
+          pkg.weightTonnes, ord.subCounty, ord.county)
+      })
+    })
+  }
 
   const overlay = document.getElementById('items-overlay')
   const itemsFragment = document.createDocumentFragment()
@@ -253,58 +425,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   clickToRemoveOverlay(overlay)
 
-  //Function to control how the drivers are displayed in the the select driver drop down
-  function displayDrivers(packageId, storageTemp, packageWt, subCnty, cnty) {
-    const selectElem = document.querySelector(`.js-driver-select-${packageId}`);
-    if (!selectElem) return;
-
-    const selectDriverFragment = document.createDocumentFragment();
-
-    orderDriverData.drivers.forEach(driver => {
-      const vehicleData = driver.vehicle
-      const { tempCaps, currentLoad, maxTons } = vehicleData
-      const { primaryZone, homeCounty } = driver
-
-      if (!tempCaps.includes(storageTemp)) {
-        return;
-      }
-
-      const totalLoad = Number((currentLoad + packageWt).toFixed(2))
-      if (totalLoad >= maxTons) {
-        return;
-      }
-
-      const zoneMatch = primaryZone.toLowerCase() === subCnty.toLowerCase();
-      const countyMatch = homeCounty.toLowerCase() === cnty.toLowerCase();
-      const option = document.createElement('option');
-      option.value = driver.driverId;
-
-      let status = ''
-      if (zoneMatch && countyMatch) {
-        status = '🟢Zone 🟢County'
-      } else if (zoneMatch) {
-        status = '🟢Zone 🔵County'
-      } else if (countyMatch) {
-        status = '🔵Zone 🟢County'
-      } else {
-        status = '🔵Zone 🔵County'
-      }
-
-      option.textContent = `${driver.name} @ ${currentLoad}t | ${status}`;
-      selectDriverFragment.appendChild(option)
-    })
-
-    selectElem.appendChild(selectDriverFragment)
-  }
-
-  orderDriverData['dispatchQueue'].forEach(ord => {
-    ord['packages'].forEach(pkg => {
-      displayDrivers(pkg.packageId, pkg.storageTemp,
-        pkg.weightTonnes, ord.subCounty, ord.county)
-    })
-  })
-
-
+  //Displays the notification message to assign dispatch driver
   function displayDispatchNotif() {
     const overlay = document.getElementById('dispatch-overlay')
     const dispatchOrgElem = document.querySelector('.js-dispatch-org-name')
@@ -342,10 +463,89 @@ document.addEventListener('DOMContentLoaded', () => {
 
         clickToRemoveOverlay(overlay)
       })
-    
+
   }
 
   displayDispatchNotif()
 
 
+  // ##Code that handles the filtering logic
+  function searchOrders(searchText, selectedCounty, selectedZone, selectedTemp, orders) {
+    const searchTerm = searchText.toLowerCase().trim()
+
+    return orders.map(order => {
+      const countyMatch = selectedCounty === `` || order.county === selectedCounty
+      const zoneMatch = selectedZone === '' || order.subCounty === selectedZone
+
+      const hospitalMatch = order.institutionName.toLowerCase().includes(searchTerm)
+
+      const filterdPackages = order.packages.filter(pkg => {
+        const tempMatch = selectedTemp === '' || pkg.packageId.endsWith(`-${selectedTemp}`)
+
+        const idMatch = searchTerm === '' || pkg.packageId.toLowerCase().includes(searchTerm)
+
+        return zoneMatch && countyMatch && tempMatch && (idMatch || hospitalMatch)
+      })
+
+      if (filterdPackages.length > 0) {
+        return { ...order, packages: filterdPackages }
+      }
+      return null
+    }).filter(order => order !== null)
+  }
+
+  const searchInputElem = document.getElementById('idSearch')
+  const countyDropdown = document.getElementById('countyFilter')
+  const zoneDropdown = document.getElementById('zoneFilter')
+
+  let currentTempFilter = ``
+  document.querySelectorAll('.t-btn')
+    .forEach(button => {
+      button.addEventListener('click', () => {
+        document.querySelectorAll('.t-btn').forEach(b => b.classList.remove('active'));
+        button.classList.add('active')
+
+        currentTempFilter = button.dataset.temp
+
+        updateHandler()
+      })
+    })
+
+  //Displays the filtered results in the page
+  function updateHandler() {
+    const filteredResults = searchOrders(
+      searchInputElem.value,
+      countyDropdown.value,
+      zoneDropdown.value,
+      currentTempFilter,
+      orderDriverData['dispatchQueue']
+    )
+    dispatchGridElem.innerHTML = ``
+
+    filteredResults.forEach(order => {
+      order.packages.forEach(pkg => {
+        displayPackages(pkg, order)
+      })
+    })
+
+    pkgToShipElem.textContent = filteredResults.length
+
+    dispatchGridElem.appendChild(dispatchOdersFragment)
+    displayRightDrivers(orderDriverData)
+  }
+
+  searchInputElem.addEventListener('input', updateHandler)
+  countyDropdown.addEventListener('change', updateHandler)
+  zoneDropdown.addEventListener('change', updateHandler)
+
+  document.querySelector('.js-reset-btn')
+    .addEventListener('click', () => {
+      searchInputElem.value = ``
+      countyDropdown.value = ``
+      zoneDropdown.value = ``
+      document.querySelectorAll('.t-btn').forEach(b => b.classList.remove('active'));
+
+      displayAllPackages(orderDriverData)
+    })
+  // ##End of code that handles the filtering logic
 })
