dc92e397542b44dd371b475a4930a2dabd9b93af
Max <mugunamaxwel@gmail.com>
2026-01-20 06:40:05 +0300

    Set up the overlay to show up when delete button is clicked

--- frontend/wh_manager/stock/wh_layout.js
+++ frontend/wh_manager/stock/wh_layout.js
@@ -1,5 +1,18 @@
 import { renderSidebar } from "../sidebar.js";
+import { xRemoveOverlay, clickToRemoveOverlay } from "../overlay.js";
 
 document.addEventListener('DOMContentLoaded', () => {
   renderSidebar()
+
+  const overlay = document.getElementById('assignmentModal')
+
+  document.getElementById('shelfMasterTable')
+    .addEventListener('click', (e) => {
+      if(e.target.classList.contains('delete-shelf')){
+        overlay.classList.add('active')
+
+        xRemoveOverlay(overlay)
+        clickToRemoveOverlay(overlay)
+      }
+    })
 })
