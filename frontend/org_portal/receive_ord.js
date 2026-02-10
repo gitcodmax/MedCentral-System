658b59f70a1dd8ab1237a882bcb8bb25e4520226
Max <mugunamaxwel@gmail.com>
2026-02-10 10:03:33 +0300

    Design the package details overlay and set up the button to display it

--- frontend/org_portal/receive_ord.js
+++ frontend/org_portal/receive_ord.js
@@ -1,5 +1,17 @@
 import { renderSidebar } from "./sidebar.js"
+import { handleOverlay } from "./overlay.js"
 
 document.addEventListener('DOMContentLoaded', () => {
   renderSidebar()
+
+  document.getElementById('packagesTbody')
+    .addEventListener('click', (e) => {
+      const btn = e.target.closest('button')
+      if(!btn) return;
+
+      if(btn.classList.contains('view-pkg-btn')){
+        const overlayElem = document.getElementById('packageDetailsOverlay')
+        handleOverlay(overlayElem)
+      }
+    })
 })
