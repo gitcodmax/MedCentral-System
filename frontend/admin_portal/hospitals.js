d449e55727d3cfb0da6a6db070178a2fd18caa77
Max <mugunamaxwel@gmail.com>
2026-02-20 07:50:12 +0300

    Set up the add new hospital button to open overlay and buttons to  close the overlay

--- frontend/admin_portal/hospitals.js
+++ frontend/admin_portal/hospitals.js
@@ -3,9 +3,27 @@ import { renderSidebar } from "./sidebar.js"
 document.addEventListener('DOMContentLoaded', () => {
   renderSidebar()
   document.querySelector('.js-header-left')
-  .innerHTML = `
+    .innerHTML = `
     <div class="page-title">
       <h2>Hospital Management</h2>
     </div> 
   `
+
+  // Set up the overlay
+  const overlayElem = document.getElementById('addHospitalOverlay')
+  document.getElementById('btnAddHospital')
+    .addEventListener('click', () => {
+      overlayElem.classList.add('active')
+
+      document.querySelectorAll('.close-overlay')
+        .forEach(btn => {
+          btn.addEventListener('click', () => {
+            overlayElem.classList.remove('active')
+          })
+        })
+
+      overlayElem.addEventListener('click', (e) => {
+        if (e.target === overlayElem) overlayElem.classList.remove('active')
+    })
+    })
 })
