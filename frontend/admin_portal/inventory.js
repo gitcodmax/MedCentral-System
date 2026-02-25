afb5e0def1ca98e080626dccfc1023b75e647c8a
Max <mugunamaxwel@gmail.com>
2026-02-25 21:52:58 +0300

    Add tabs in system configuration to add bulk units and selling units

--- frontend/admin_portal/inventory.js
+++ frontend/admin_portal/inventory.js
@@ -26,51 +26,91 @@ document.addEventListener('DOMContentLoaded', () => {
     .addEventListener('click', () => {
       handleOverlay(configOverlayElem)
 
+      const categoriesListElem = document.getElementById('categoriesList')
+      const storageTempListElem = document.getElementById('storageTempList')
+      const bulkUnitsListElem = document.getElementById('bulkUnitsList')
+      const sellUnitsListElem = document.getElementById('sellUnitsList')
+
       // Hide the storage temp list at first
       if (document.querySelector('.js-categories-tab-btn')
         .classList.contains('active')
       ) {
-        document.getElementById('storageTempList')
-          .style.display = 'none'
+        bulkUnitsListElem.style.display = 'none'
+        sellUnitsListElem.style.display = 'none'
+        storageTempListElem.style.display = 'none'
       }
 
       configOverlayElem.addEventListener('click', (e) => {
-        const categoriesListElem = document.getElementById('categoriesList')
-        const storageTempListElem = document.getElementById('storageTempList')
+        const categoriesTabBtnElem = document.querySelector('.js-categories-tab-btn')
+        const storageTabBtnElem = document.querySelector('.js-storage-tab-btn')
+        const bulkUnitsTabBtnElem = document.querySelector('.js-bulk-unit-tab-btn')
+        const sellUnitsTabBtnElem = document.querySelector('.js-sell-unit-tab-btn')
         const newValueInputElem = document.getElementById('newValueInput')
 
-        // Toggle tab btn
+        // Toggle storage temp tab btn
         if (e.target.classList.contains('js-storage-tab-btn')) {
           e.target.classList.add('active')
-          document.querySelector('.js-categories-tab-btn')
-            .classList.remove('active')
+          categoriesTabBtnElem.classList.remove('active')
+          bulkUnitsTabBtnElem.classList.remove('active')
+          sellUnitsTabBtnElem.classList.remove('active')
 
           categoriesListElem.style.display = 'none'
+          bulkUnitsListElem.style.display = 'none'
+          sellUnitsListElem.style.display = 'none'
           storageTempListElem.style.display = 'flex'
         }
 
-        // Toggle tab btn
+        // Toggle item categories tab btn
         if (e.target.classList.contains('js-categories-tab-btn')) {
           e.target.classList.add('active')
-          document.querySelector('.js-storage-tab-btn')
-            .classList.remove('active')
+          storageTabBtnElem.classList.remove('active')
+          bulkUnitsTabBtnElem.classList.remove('active')
+          sellUnitsTabBtnElem.classList.remove('active')
 
           categoriesListElem.style.display = 'flex'
+          bulkUnitsListElem.style.display = 'none'
+          sellUnitsListElem.style.display = 'none'
+          storageTempListElem.style.display = 'none'
+        }
+
+        // Toggle bulk units tab btn
+        if(e.target.classList.contains('js-bulk-unit-tab-btn')){
+          e.target.classList.add('active')
+          storageTabBtnElem.classList.remove('active')
+          categoriesTabBtnElem.classList.remove('active')
+          sellUnitsTabBtnElem.classList.remove('active')
+
+          bulkUnitsListElem.style.display = 'flex'
+          categoriesListElem.style.display = 'none'
+          sellUnitsListElem.style.display = 'none'
+          storageTempListElem.style.display = 'none'
+        }
+
+        // Toggle sell units tab btn
+        if(e.target.classList.contains('js-sell-unit-tab-btn')){
+          e.target.classList.add('active')
+          storageTabBtnElem.classList.remove('active')
+          categoriesTabBtnElem.classList.remove('active')
+          bulkUnitsTabBtnElem.classList.remove('active')
+
+          bulkUnitsListElem.style.display = 'none'
+          categoriesListElem.style.display = 'none'
+          sellUnitsListElem.style.display = 'flex'
           storageTempListElem.style.display = 'none'
         }
 
         // Adding a new value for categories or storage temperature
         if (e.target.classList.contains('js-add-new-value')) {
-          if(newValueInputElem.value === ``){
+          if (newValueInputElem.value === ``) {
             alert('Enter a new value')
-          }else{
+          } else {
             if (document.querySelector('.js-categories-tab-btn')
               .classList.contains('active')
             ) {
               console.log('Categories Active')
               // Save the new category and display them in the categories list
             }
-  
+
             if (document.querySelector('.js-storage-tab-btn')
               .classList.contains('active')
             ) {
@@ -98,22 +138,22 @@ document.addEventListener('DOMContentLoaded', () => {
     .addEventListener('click', (e) => {
 
       // View item details
-      if(e.target.classList.contains('fa-eye')){
+      if (e.target.classList.contains('fa-eye')) {
         handleOverlay(viewItemOverlayElem)
       }
 
       // Edit item details
-      if(e.target.classList.contains('fa-edit')){
+      if (e.target.classList.contains('fa-edit')) {
         handleOverlay(editItemOverlayElem)
       }
 
       // Adjust the stock amount
-      if(e.target.classList.contains('fa-balance-scale')){
+      if (e.target.classList.contains('fa-balance-scale')) {
         handleOverlay(adjustStockOverlayElem)
       }
 
       // Delete an item
-      if(e.target.classList.contains('fa-trash')){
+      if (e.target.classList.contains('fa-trash')) {
         handleOverlay(deleteItemOverlayElem)
       }
     })
