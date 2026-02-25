1fd6e2383899940bbd2d9dfbe735563d361919b9
Max <mugunamaxwel@gmail.com>
2026-02-25 19:32:39 +0300

    Set up the overlay that defines the item categories and storage temperature.

--- frontend/admin_portal/inventory.js
+++ frontend/admin_portal/inventory.js
@@ -1,9 +1,10 @@
 import { renderSidebar } from "./sidebar.js";
+import { handleOverlay } from "../global.js";
 
 document.addEventListener('DOMContentLoaded', () => {
   renderSidebar()
   document.querySelector('.js-header-left')
-  .innerHTML = `
+    .innerHTML = `
     <div class="dashboard-header">
       <div class="page-title">
         <h1>Inventory Management</h1>
@@ -18,4 +19,66 @@ document.addEventListener('DOMContentLoaded', () => {
       </div>
     </div> 
   `
+
+  const configOverlayElem = document.getElementById('configOverlay')
+  document.getElementById('openConfigBtn')
+    .addEventListener('click', () => {
+      handleOverlay(configOverlayElem)
+
+      // Hide the storage temp list at first
+      if (document.querySelector('.js-categories-tab-btn')
+        .classList.contains('active')
+      ) {
+        document.getElementById('storageTempList')
+          .style.display = 'none'
+      }
+
+      configOverlayElem.addEventListener('click', (e) => {
+        const categoriesListElem = document.getElementById('categoriesList')
+        const storageTempListElem = document.getElementById('storageTempList')
+        const newValueInputElem = document.getElementById('newValueInput')
+
+        // Toggle tab btn
+        if (e.target.classList.contains('js-storage-tab-btn')) {
+          e.target.classList.add('active')
+          document.querySelector('.js-categories-tab-btn')
+            .classList.remove('active')
+
+          categoriesListElem.style.display = 'none'
+          storageTempListElem.style.display = 'flex'
+        }
+
+        // Toggle tab btn
+        if (e.target.classList.contains('js-categories-tab-btn')) {
+          e.target.classList.add('active')
+          document.querySelector('.js-storage-tab-btn')
+            .classList.remove('active')
+
+          categoriesListElem.style.display = 'flex'
+          storageTempListElem.style.display = 'none'
+        }
+
+        // Adding a new value for categories or storage temperature
+        if (e.target.classList.contains('js-add-new-value')) {
+          if(newValueInputElem.value === ``){
+            alert('Enter a new value')
+          }else{
+            if (document.querySelector('.js-categories-tab-btn')
+              .classList.contains('active')
+            ) {
+              console.log('Categories Active')
+              // Save the new category and display them in the categories list
+            }
+  
+            if (document.querySelector('.js-storage-tab-btn')
+              .classList.contains('active')
+            ) {
+              console.log('Storage Active')
+              // Save the new category and display them in the storage list
+            }
+          }
+        }
+      })
+
+    })
 })
