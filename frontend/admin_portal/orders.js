daba5b54330f581e51d08ad8b454ae483fa0f8f8
Max <mugunamaxwel@gmail.com>
2026-02-26 22:27:39 +0300

    Remove the update status button and overlay. Get rid of update status feature.

--- frontend/admin_portal/orders.js
+++ frontend/admin_portal/orders.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('.js-header-left')
     .innerHTML = `
       <div class="page-title">
-        <h2>Order & Request Management</h2>
+        <h2>Order & Request History</h2>
       </div> 
     `
 
@@ -18,11 +18,6 @@ document.addEventListener('DOMContentLoaded', () => {
     if(btn.classList.contains('js-view-ord-details-btn')){
       const orderDetailsOverlayElem = document.getElementById('orderDetailsOverlay')
       handleOverlay(orderDetailsOverlayElem)
-    }
-
-    if(btn.classList.contains('js-update-status-btn')){
-      const updateStatusOverlayElem = document.getElementById('updateStatusOverlay')
-      handleOverlay(updateStatusOverlayElem)
     }
   })
 })
